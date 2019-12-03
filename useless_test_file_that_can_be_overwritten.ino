a4d1614d8c1001b954d259383d033427f19ea5ed
Alexander Dukakis <57136679+ASDukakis@users.noreply.github.com>
2019-12-03 16:54:15 +0000

    actually useful now

--- useless_test_file_that_can_be_overwritten.ino
+++ useless_test_file_that_can_be_overwritten.ino
@@ -7,20 +7,21 @@ SerialChecker scpc(Serial);
 uStepperS stepper;
 
 float velocity = 100;
-float maxvelocity = 300;
+float maxvelocity = 100;
 float angle = 0;
 float distance = 0;
 
 bool upstop = true;
 bool upstopOld = true;
+
 bool downstop = true;
 bool downstopOld = true;
 
 bool topLED = false;
 bool botLED = false;
 
-bool direc = false;   //0 for upward, 1 for downward
-bool direcOld = false;
+bool direc = true;   //0 for downward, 1 for upward
+bool direcOld = true;
 
 MilliTimer DebounceTimer(25);
 MilliTimer ZeroTimeout(15000);
@@ -60,9 +61,9 @@ void loop() {
         motorstate = states::IDLE;
         scpc.println("bouncymotor returned to idle");
         scpc.print("Angle: ");
-        scpc.println(stepper.encoder.getAngle());
+        scpc.println(stepper.encoder.getAngleMoved());
         scpc.print("Microsteps: ");
-        scpc.println(stepper.driver.getPosition());
+        scpc.println(-1 * stepper.driver.getPosition());
       }
       break;
   }
@@ -86,27 +87,27 @@ void zeroing(bool reset = false){
     case 1:
       // check we haven't already hit the endstop
       if(upstop == true){
-        direc = 0;
+        direc = 1;
         stepper.setMaxVelocity(maxvelocity);
-        stepper.runContinous(0);
+        stepper.runContinous(1);
       }
       zStep++;
       break;
     case 2:
       if(upstop == false){
-        direc = 1;
+        direc = 0;
         // make motor start moving 500 steps downwards
         stepper.setMaxVelocity(50);
-        stepper.moveSteps(12000);
+        stepper.moveSteps(-15000);
         ZeroTimeout.reset();
         zStep++;
       }
       break;
     case 3:
       if((stepper.getMotorState() == 0) && (upstop == true)){
-        direc = 0;
+        direc = 1;
         stepper.setMaxVelocity(25);
-        stepper.moveSteps(-20000);
+        stepper.moveSteps(20000);
         ZeroTimeout.reset();
         zStep++;      
       }
@@ -117,9 +118,11 @@ void zeroing(bool reset = false){
         stepper.encoder.setHome();
         ZeroTimeout.reset();
         zStep = 1;
-        direc = 1;
+        direc = 0;
         motorstate = states::IDLE;
         scpc.println("Zeroing complete.");
+        scpc.print("Angle: ");
+        scpc.println(stepper.encoder.getAngleMoved());
       }
       break;
   }
@@ -130,48 +133,50 @@ void checkSerialPC(){
     if(scpc.contains("kek")){
       scpc.println("nice work everyone!"); 
     }
-  }
-  else if(scpc.contains("ze")){ // zero the stage to top microswitch
-    direcOld = direc;
-    zeroing(true);
-    motorstate = states::ZEROING;
-    scpc.println("Zeroing...");
-  } 
-  else if(scpc.contains("su")){ // set direc to up
-    if(direc == 1){
-      direc = 0;
-      scpc.println("direction set to: up");
-      if(motorstate == states::MOVING){
-        stepper.stop(HARD);
-        stepper.runContinous(direc);
+    else if(scpc.contains("ze")){ // zero the stage to top microswitch
+      direcOld = direc;
+      zeroing(true);
+      motorstate = states::ZEROING;
+      scpc.println("Zeroing...");
+    } 
+    else if(scpc.contains("su")){ // set direc to up
+      if(direc == 0){
+        direc = 1;
+        scpc.println("direction set to: up");
+        if(motorstate == states::MOVING){
+          stepper.stop(HARD);
+          stepper.runContinous(direc);
+       }
       }
-    }
-    else if(direc == 0){
-      scpc.println("Already moving upward.");
-    }
-  }
-  else if(scpc.contains("sd")){ // set direc to down
-    if(direc == 0){
-      direc = 1;
-      scpc.println("direction set to: down");
-      if(motorstate == states::MOVING){
-        stepper.stop(HARD);
-        stepper.runContinous(direc);
+      else if(direc == 1){
+       scpc.println("Already moving upward.");
       }
+   }
+   else if(scpc.contains("sd")){ // set direc to down
+      if(direc == 1){
+        direc = 0;
+        scpc.println("direction set to: down");
+        if(motorstate == states::MOVING){
+          stepper.stop(HARD);
+          stepper.runContinous(direc);
+       }
+     }
+      else if(direc == 0){
+        scpc.println("Already moving downward.");
+     }
     }
-    else if(direc == 1){
-      scpc.println("Already moving downward.");
-    }
+    else if(scpc.contains("ms")){
+     motorstate = states::MOVING;
+      if(direc == 1){
+        stepper.moveSteps(51200);
+        stepper.setMaxVelocity(100);
+      }
+      else if(direc == 0){
+       stepper.moveSteps(-51200);
+       stepper.setMaxVelocity(100);
+      }
+    } 
   }
-  else if(scpc.contains("ms")){
-    motorstate = states::MOVING;
-    if(direc == 0){
-      stepper.moveSteps(-51200);
-    }
-    else if(direc == 1){
-      stepper.moveSteps(51200);
-    }
-  } 
 }
 
 void checkEndStops(){
@@ -194,7 +199,7 @@ void checkEndStops(){
 }
 
 void stopMotorTop(){
-  if(direc == 0){
+  if(direc == 1){
     stepper.stop(HARD);
     topLED = true;
     if(motorstate == states::MOVING){
@@ -205,7 +210,7 @@ void stopMotorTop(){
 }
 
 void stopMotorBot(){
-  if(direc == 1){
+  if(direc == 0){
     stepper.stop(HARD);
     botLED = true;     //change to digitalwrite later?
     if(motorstate == states::MOVING){
